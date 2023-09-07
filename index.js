548491d1c5e5cebfbf10a9872f4471d9192f6069
Fralou Maksim <fralou@protonmail.com>
2023-09-07 11:05:25 +0300

    fix: switchLang

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const langEng = document.querySelector('.container-eng');
 function switchLanguageToEng() {
     langRu.classList.add('hidden');
     langEng.classList.remove('hidden');
+    BTN_ENG.classList.add('switch-lang-eng');
+    BTN_RU.classList.remove('switch-lang-ru');
 }
 
 function switchLanguageToRu() {
     langRu.classList.remove('hidden');
     langEng.classList.add('hidden');
+    BTN_RU.classList.add('switch-lang-ru');
+    BTN_ENG.classList.remove('switch-lang-eng');
 }
 
 BTN_ENG.addEventListener('click', switchLanguageToEng);
