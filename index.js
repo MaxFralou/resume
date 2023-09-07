ee24e136bac8a83b7645599d42f2b651edf4ee7d
Fralou Maksim <fralou@protonmail.com>
2023-09-07 16:52:33 +0300

    fix: fix and refactor

--- index.js
+++ index.js
@@ -3,19 +3,37 @@ const BTN_ENG = document.getElementById('eng');
 const langRu = document.querySelector('.container-ru');
 const langEng = document.querySelector('.container-eng');
 
-function switchLanguageToEng() {
-    langRu.classList.add('hidden');
-    langEng.classList.remove('hidden');
-    BTN_ENG.classList.add('switch-lang-eng');
-    BTN_RU.classList.remove('switch-lang-ru');
-}
-
-function switchLanguageToRu() {
-    langRu.classList.remove('hidden');
-    langEng.classList.add('hidden');
-    BTN_RU.classList.add('switch-lang-ru');
-    BTN_ENG.classList.remove('switch-lang-eng');
-}
-
-BTN_ENG.addEventListener('click', switchLanguageToEng);
-BTN_RU.addEventListener('click', switchLanguageToRu);
+const langs = {
+    ru: langRu,
+    eng: langEng
+  };
+  
+  const buttons = {
+    ru: BTN_RU,
+    eng: BTN_ENG 
+  };
+  
+  function switchLanguage(lang) {
+    hideOtherLanguage(lang);
+    showLanguage(lang);
+    setActiveButton(lang);
+  }
+  
+  function hideOtherLanguage(activeLang) {
+    const inactiveLang = activeLang === 'ru' ? 'eng' : 'ru';
+    langs[inactiveLang].classList.add('hidden');
+  }
+  
+  function showLanguage(lang) {
+    langs[lang].classList.remove('hidden'); 
+  }
+  
+  function setActiveButton(lang) {
+    buttons[lang].classList.add('flag-lang');
+    
+    const inactiveLang = lang === 'ru' ? 'eng' : 'ru';
+    buttons[inactiveLang].classList.remove('flag-lang');
+  }
+  
+  buttons.ru.addEventListener('click', () => switchLanguage('ru'));
+  buttons.eng.addEventListener('click', () => switchLanguage('eng'));
